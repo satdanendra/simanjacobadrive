dc9fcb864677adef4d95792e5db8f28bc680d19f
222112354 <danendra3671@gmail.com>
2025-05-18 13:34:00 +0700

    tambah tombol tim

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('tim')" :active="request()->routeIs('tim')">
+                        {{ __('Tim') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
